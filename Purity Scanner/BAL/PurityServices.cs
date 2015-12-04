ba316a89c2ba04f2ea4769983be81c4e12518d1d
ravikumarh <ravikumarh@tnex.co.in>
2015-12-04 19:13:20 +0530

    Purity Scanner Service BAL Commit
    First time BAL commit

---
+++ Purity Scanner/BAL/PurityServices.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PL;
+using DAL;
+
+namespace BAL
+{
+   public class PurityServices
+    {
+       DBAccess obj = new DBAccess();
+       public AppMetaDataResponce getAppMetadata(string SecurityKey)
+       {
+           return obj.getAppMetadata(SecurityKey);
+       }
+
+       public ProductDetailsResponce getAllProductsByIDs(ProductsByIDsRequest productRequestData)
+       {
+           return obj.getAllProductsByIDs(productRequestData);
+       }
+
+       public ProductDetailsResponce getProductDetailsByImageKey(ProductDetailsResquest productDetailsRequestData)
+       {
+           return obj.getProductDetailsByImageKey(productDetailsRequestData);
+       }
+
+       public ManifestoResponce GetManifesto(ManifestoRequest manifestoRequestData)
+       {
+           return obj.GetManifesto(manifestoRequestData);
+       }
+    }
+}
